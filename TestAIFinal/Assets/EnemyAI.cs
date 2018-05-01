2476b875dec9c61a50385e9f4c01ea410a70c6fe
Rae <Rae.vroman@gmail.com>
2018-05-01 13:59:23 -0400

    added stufs

---
+++ TestAIFinal/Assets/EnemyAI.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    Animator anim;
+    CharacterController charController;
+    public Collider spearCollider;
+    public float speed = 2.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    public GameObject player;
+    public Collider platform;
+    private bool canJump = true;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (charController.isGrounded)
+        {
+            transform.LookAt(player.transform.position);
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Random.Range(0,2.0f), 0, Random.Range(0,2.0f));
+            anim.SetBool("running", true);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            StartCoroutine(Jump(Random.Range(2, 6)));
+            canJump = true;
+
+        }
+
+        //If enemy has fallen off platform
+        else if (canJump && !charController.isGrounded && (transform.position.x > 11 || transform.position.x < -11 || (transform.position.z > 11 || transform.position.z < -11)))
+        {
+            anim.SetBool("running", false);
+            transform.LookAt(player.transform.position);
+            if (transform.position.x > 11)
+            {
+                moveDirection = new Vector3(-2, 0, 0);
+
+            }
+            else if (transform.position.x < -11)
+            {
+                moveDirection = new Vector3(2, 0, 0);
+
+            }
+            else if (transform.position.z > 11)
+            {
+                moveDirection = new Vector3(0, 0, -2);
+
+            }
+            else if (transform.position.z < -11)
+            {
+                moveDirection = new Vector3(0, 0, 2);
+
+            }
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            Invoke("JumpOnce", .15f);
+            canJump = false;
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        charController.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    private void LaunchAttack(Collider col) {
+        Collider[] cols = Physics.OverlapSphere(col.bounds.center, 2, LayerMask.GetMask("Fight"));
+        foreach (Collider c in cols) {
+
+         //   if (c.transform.parent.parent = transform)
+         //   {
+        //   }
+          
+                float damage = 10;
+                c.SendMessageUpwards("TakeDamage", damage);
+            
+
+        }
+    }
+
+    void JumpOnce() {
+
+        moveDirection.y = 7;
+    }
+    IEnumerator Jump(float jumpPower) {
+
+        moveDirection.y = jumpPower;
+        anim.SetBool("jumping", true);
+        yield return new WaitForSeconds(Random.Range(5.0f, 7.5f));
+    }
+}
---
+++ TestAIFinal/Assets/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    //public Image currentHealthBar;
+    //public Text rationText;
+
+    private float hitpoints = 150;
+    private float maxHitPoints = 150;
+
+	// Use this for initialization
+	void Start () {
+    //    UpdateHealthbar();
+	}
+
+    private void UpdateHealthbar() {
+//        float ratio = hitpoints / maxHitPoints;
+  //      currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void TakeDamage(float damage) {
+        hitpoints -= damage;
+        print(hitpoints);
+        if (hitpoints < 0) {
+            hitpoints = 0;
+            Debug.Log("Dead!");
+        }
+    }
+
+    private void HealDamage(float heal)
+    {
+        hitpoints += heal;
+        if (hitpoints > maxHitPoints)
+        {
+            hitpoints = maxHitPoints;
+            Debug.Log("Healed!!");
+        }
+    }
+}
